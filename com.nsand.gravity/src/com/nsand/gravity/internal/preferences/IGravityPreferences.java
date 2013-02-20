ac3d50fbb8c80879c08ecf7c8046255c6fde4380
nsand <nick.sandonato@gmail.com>
2013-02-20 00:20:27 -0500

    Gravity preferences

---
+++ com.nsand.gravity/src/com/nsand/gravity/internal/preferences/IGravityPreferences.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2013 Nick Sandonato
+ * 
+ * Released under the MIT license (http://opensource.org/licenses/MIT)
+ */
+package com.nsand.gravity.internal.preferences;
+
+/**
+ * @author nsand
+ *
+ */
+public interface IGravityPreferences {
+	String PROJECT_NATURES_KEY = "com.nsand.gravity.project_natures"; //$NON-NLS-1$
+}
