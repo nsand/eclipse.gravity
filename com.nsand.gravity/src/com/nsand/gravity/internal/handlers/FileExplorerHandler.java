8c6ff69294669e6513029977c4f4257380b1f995
nsand <nick.sandonato@gmail.com>
2013-02-20 22:40:36 -0500

    Create command, menu contributions, and handler

---
+++ com.nsand.gravity/src/com/nsand/gravity/internal/handlers/FileExplorerHandler.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2013 Nick Sandonato
+ * 
+ * Released under the MIT license (http://opensource.org/licenses/MIT)
+ */
+package com.nsand.gravity.internal.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class FileExplorerHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+}
